5d830dcaf105f9a88b17cadb5d45f134d4eb3f73
Alisha Khatri <alisha.191502@ncit.edu.np>
2024-09-05 10:37:12 +0545

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,5 +2,12 @@ import Title from "@/components/Title";
 import React from "react";
 
 export default function page() {
-  return <Title title="About" />;
+  return;
+  <>
+    <Title title="About" />
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  </>;
 }
